4d64b4050d353b080b21ff10cfc3ed976f63d983
eborchardt <56270998+eborchardt@users.noreply.github.com>
2022-03-16 03:43:52 -0500

    Create scrollLongMessage.ino

---
+++ scrollLongMessage.ino
@@ -0,0 +1,40 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+int x, minX;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setTextWrap(false);
+  x = display.width();
+//  minX = -12 * strlen(message); //12 = 6 pixels/character * text size 2
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char text[] = "The quick brown fox jumped over the lazy dogs back";
+  scrollText(text);
+}
+
+void scrollText(char message[]) {
+    minX = -12 * strlen(message); //12 = 6 pixels/character * text size 2
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(x,7);
+  display.print(message);
+  display.display();
+  x = x - 2; // scroll speed
+  if (x < minX) x = display.width();
+}
